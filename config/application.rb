8efde74fed2654e6b296c996aeada401eaa7eb4b
apple <test@example.com>
2021-05-02 11:22:22 +0900

    52

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = true
 
 module DeviseName
   class Application < Rails::Application
